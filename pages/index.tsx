ad4f950871c74e14f4194261e278073d2bd863ad
Frank CH Lu <frank_ch_lu@trendmicro.com>
2021-12-23 13:56:29 +0800

    update Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../components/Layout'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
+  // just for mock the huge content
+  const repeats = new Array(100).fill(0);
   return (
     <Layout title="Home | Next.js + TypeScript Example">
-    <div className="text-3xl font-bold underline">
-      Hello world! Test
-    </div>
-  </Layout>
-  )
-}
+      {repeats.map((x) => (
+        <>
+          <div className="text-3xl font-bold underline">Hello world! Test</div>
+        </>
+      ))}
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
